ae108d8f38b04d41fb91d47397cf7be0ee64bafe
annoinspace <aneesahalmask@gmail.com>
2022-11-11 13:15:55 +0000

    back office put working

--- netflix-ui-main/js/backoffice.js
+++ netflix-ui-main/js/backoffice.js
@@ -1,32 +1,34 @@
-window.onload = async () => {
-  async function onFormSubmit(event, form) {
-    event.preventDefault()
+async function onFormSubmit(event) {
+  event.preventDefault()
 
-    const newMovie = {
-      name: document.querySelector("#name").value,
-      description: document.querySelector("#description").value,
-      genre: document.querySelector("#genre").toLowerCase().value,
-      imageUrl: document.querySelector("#imageUrl").value
-    }
+  const newMovie = {
+    name: document.querySelector("#name").value,
+    description: document.querySelector("#description").value,
+    category: document.querySelector("#genre").value,
+    imageUrl: document.querySelector("#imageUrl").value
+  }
 
-    const options = {
-      method: "POST",
-      body: JSON.stringify(newMovie),
-      headers: {
-        "Content-Type": "application/json"
-      }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(newMovie),
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZlMTk5Y2Y4MGYxYTAwMTVkOGEwNDAiLCJpYXQiOjE2NjgxNTk5MDAsImV4cCI6MTY2OTM2OTUwMH0.w4pMIn-xdzD1sPgqoESBbbpirfquHjeifJ0TmqSCBSk",
+      "Content-Type": "application/json"
     }
+  }
 
-    try {
-      const endpoint = "https://striveschool-api.herokuapp.com/api/movies"
-      const response = await fetch(endpoint, options)
-      if (response.ok) {
-        alert("new movie added")
-      } else {
-        throw new Error("Error while creating new movie")
-      }
-    } catch (error) {
-      console.log(error)
+  //   const moviegenre = document.querySelector("#genre").value
+
+  try {
+    const endpoint = "https://striveschool-api.herokuapp.com/api/movies/"
+    const response = await fetch(endpoint, options)
+    if (response.ok) {
+      alert("new movie added")
+    } else {
+      throw new Error("Error while creating new movie")
     }
+  } catch (error) {
+    console.log(error)
   }
 }
