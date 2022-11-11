60706a13b4aabbee7c4c471b5f118568ecab67b2
annoinspace <aneesahalmask@gmail.com>
2022-11-11 11:47:19 +0000

    initial commit

---
+++ netflix-ui-main/js/backoffice.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/movies", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZlMTk5Y2Y4MGYxYTAwMTVkOGEwNDAiLCJpYXQiOjE2NjgxNTk5MDAsImV4cCI6MTY2OTM2OTUwMH0.w4pMIn-xdzD1sPgqoESBbbpirfquHjeifJ0TmqSCBSk"
+  }
+})
